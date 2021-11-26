dc97eb02bcddc1d6fcafeab73a9c4e75349521c2
AndresOlivaDenis <56798831+AndresOlivaDenis@users.noreply.github.com>
2021-11-26 12:59:34 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='sec_edgar',
+      version='0.0.1',
+      description='US Securities and Exchange, Electronic Data Gathering, Analysis, and Retrieval system',
+      author='R.A.O.D',
+      packages=find_packages(),
+      include_package_data=True)
